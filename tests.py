621f351001e98835e324484e2b4ae273c2dfbed0
Wok <woctezuma@users.noreply.github.com>
2023-02-06 00:54:35 +0100

    Reformatting with black

--- tests.py
+++ tests.py
@@ -4,7 +4,6 @@ import random_agent
 
 
 class TestRandomAgentMethods(unittest.TestCase):
-
     def test_run(self):
         self.assertTrue(random_agent.run(display_on_screen=False, num_steps=400))
 

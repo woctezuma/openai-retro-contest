eb95e311797961f5661a7065905a8fe06b27533d
Wok <woctezuma@users.noreply.github.com>
2023-02-06 00:55:50 +0100

    Refactoring with ruff (auto-fix)

--- tests.py
+++ tests.py
@@ -5,7 +5,7 @@ import random_agent
 
 class TestRandomAgentMethods(unittest.TestCase):
     def test_run(self):
-        self.assertTrue(random_agent.run(display_on_screen=False, num_steps=400))
+        assert random_agent.run(display_on_screen=False, num_steps=400)
 
 
 if __name__ == '__main__':
